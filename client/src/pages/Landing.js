4b12a8312d8673ea59efc31c7fa1cdeac090d041
Christian <927.christian@gmail.com>
2021-03-23 20:25:33 -0500

    addition of components directory and pages directory

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Landing() {
+    return (
+        <div>
+ 
+          <h3>Create</h3>
+          <p>Create your own quiz.</p>
+        </div>      
+    )
+}
+
+export default Landing
