3d6a22d1c04bfbffbe39dc76eaa34926e416408c
darrellfr3 <darrellfr3@gmail.com>
2021-03-31 20:57:19 -0500

    saving small changes

--- config/passport.js
+++ config/passport.js
@@ -23,7 +23,7 @@ passport.use(new LocalStrategy(
         });
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
+      else if (!dbUser.comparePassword(password)) {
         return done(null, false, {
           message: "Incorrect password."
         });
