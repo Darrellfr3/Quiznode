520de4a3373983a707ab8a99876657e3690a9e6a
darrellfr3 <darrellfr3@gmail.com>
2021-04-03 11:12:49 -0500

    Added find subject method to quiz controller

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -8,6 +8,12 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findSubject: function(req, res) {
+    db.Quiz
+      .find({subject: req.query})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
   create: function(req, res) {
     db.Quiz
       .create(req.body)
