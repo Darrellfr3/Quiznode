4acdf28b8e4e3f325b21368d900ad25cc5c7eaee
Christian <927.christian@gmail.com>
2021-04-08 19:06:22 -0500

    additional home styling. Fixed links to buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Footer from "../components/footer"
-import {Row, Col, Button, Container} from "react-bootstrap";
+import { Row, Col, Button, Container } from "react-bootstrap";
 import Navbar from '../components/navbar';
-import { BrowserRouter as Router, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './userHome.css';
+import '../components/Landing/index.scss';
 
 
 
@@ -12,46 +13,54 @@ import './userHome.css';
 
 
 function Home() {
-    return (
-        <Router>
-        <Navbar/>  
+  return (
+    
+    <div id="my-background" className="background">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <Router>
+        <Navbar />
+
         <Container>
-            <Row >
-            <Col className="quizcontainer1">
-            
-          <h3> Build a Quiz</h3>
-          <p>Build a custom quiz with your own Q's {`&`} A's</p>
-          <Link to="/createQuiz">
-          <Button size="lg" block>Build Quiz</Button>{' '}
-          </Link>
 
-          </Col>
-
+          <Row >
             <Col className="quizcontainer1">
-           
-          <h3> Take a Quiz</h3>
-          <p>Take a quiz that you've built previously.</p>
-          <Link to="/search">
-          <Button size="lg" block>Take Quiz</Button>
-          </Link>
+
+              <h3> Build a Quiz</h3>
+              <p>Build a custom quiz with your own Q's {`&`} A's</p>
+
+              <Button size="lg" block href="/createQuiz">Build Quiz</Button>{' '}
+
+
             </Col>
 
             <Col className="quizcontainer1">
-         
-          <h3> Pregenerated Quiz </h3>
-          <p>Take a quiz that's been pregenerated.</p>
-          <Link to="/quizGen">
-          <Button size="lg" block>Load Quiz</Button>
-          </Link>
-          </Col>
-      
+
+              <h3> Take a Quiz</h3>
+              <p>Take a quiz that you've built previously.</p>
+
+              <Button size="lg" block href="/search">Take Quiz</Button>
+
+
+            </Col>
+
+            <Col className="quizcontainer1">
+
+              <h3> Pregenerated Quiz </h3>
+              <p>Take a quiz that's been pregenerated.</p>
+
+              <Button size="lg" block href="/createQuiz">Load Quiz</Button>
+
+            </Col>
+
 
           </Row>
-          </Container>
-          <Footer/>
-     
-        </Router>
-        
+        </Container>
+        <Footer />
+
+      </Router>
+      </div>
     )
 }
 
